b886cf367acf46ad9e3a9a57a10c06a3bc7f7eda
ebedser <ebedser@stevens.edu>
2017-04-12 17:22:56 -0400

    Added a menu after search to choose result Still need to do more input checking Should add "I'm feeling lucky" style option as argument        to speed up search if wanted

--- genius.py
+++ genius.py
@@ -10,11 +10,16 @@ import certifi
 #https://genius.com/search?q=word1+word2  <-search
 #https://genius.com/word1-word2-lyrics <- lyric at end for lyric urls
 
+def yesOrNo(text=""):
+    """Asks for user to input y or n. Returns true if y"""
+    response = input(text + " (y/n)? ")
+    return(response.lower() == "y")
+
 
 parser = argparse.ArgumentParser()
-parser.add_argument("song", help="the song for which you want lyrics")
+parser.add_argument("searchTerms", help="terms for which you are searching")
 args = parser.parse_args()
-searchList = args.song.split(" ")
+searchList = args.searchTerms.split(" ")
 searchUrl = "https://genius.com/search?q="
 for term in searchList:
     if term != "":
@@ -27,15 +32,29 @@ http = urllib3.PoolManager(
 searchPage = http.request('GET', searchUrl)
 searchPage = BeautifulSoup(searchPage.data, 'lxml')
 resultList = searchPage.find_all('a', attrs={'class': ' song_link'})
-lyricsUrl = ""
+optionList =  [] # list of possible song lyrics
+urlList = [] # list of urls
 for result in resultList:
     url = result.get('href')
     if url[len(url)-7:len(url)] == "-lyrics":
         lyricsUrl = url
-        break
+        title = result.text
+        optionList.append(title)
+        urlList.append(url)
 
-lyricsPage = http.request('GET', lyricsUrl)
-lyricsPage = BeautifulSoup(lyricsPage.data, 'lxml')
-
-lyrics = lyricsPage.find('lyrics',attrs={'class': 'lyrics'}).text
-print(lyrics)
+for i in range(len(optionList)):
+    # removes newline characters from beginning and end
+    optionList[i] = optionList[i][1:len(optionList[i])-1] 
+    print(str(i) + ") " + optionList[i])
+choice = int(input("Choose an option ( 0-" + str(len(optionList)-1) + " ): "))
+if(choice < len(optionList) and choice >= 0):
+    lyricsUrl = urlList[choice]
+    lyricsPage = http.request('GET', lyricsUrl)
+    lyricsPage = BeautifulSoup(lyricsPage.data, 'lxml')
+    lyricsPage = http.request('GET', lyricsUrl)
+    lyricsPage = BeautifulSoup(lyricsPage.data, 'lxml')
+    lyrics = lyricsPage.find('lyrics',attrs={'class': 'lyrics'}).text
+    print("\n" + optionList[choice])
+    print(lyrics)
+else:
+    print("Not a valid option")
